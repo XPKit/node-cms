17fd0eb8d823c35e3aa238162f2e15fe6c408b4b
Edouard Durand <edouard.durand@imagination.com>
2024-10-14 22:10:42 +0800

    make app unselectable/clickable if laoding

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app>
+  <v-app :class="{'unclickable': isLoading}">
     <v-theme-provider :theme="getTheme()">
       <v-snackbar
         v-model="showSnackBar" transition="scroll-y-reverse-transition" multi-line
